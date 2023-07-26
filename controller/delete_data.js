95e7ea773adfd531cebe773f33b0cf634f0b67c5
Diyorbek Anorboyev <diyorbekastrum@gmail.com>
2023-07-26 16:46:30 +0500

    use delete function

--- controller/delete_data.js
+++ controller/delete_data.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const Phone = require("../model/Phone");
 const router = Router()
 
-router.delete("/api/restaurant/:id", async(req, res) => {
+router.delete("/api/phone/:id", async(req, res) => {
     await Phone.findByIdAndDelete(req.params.id)
     res.send({msg: "Success deleted"})
   })
